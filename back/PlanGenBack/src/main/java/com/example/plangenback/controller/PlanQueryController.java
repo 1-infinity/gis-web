f3003e4e37da471f11eac8c0f8bff3e87558e042
merlin <104957202+1-infinity@users.noreply.github.com>
2024-04-09 13:06:42 +0800

    add cross origin (#2)
    Co-authored-by: Inception02 <127276038+Inception02@users.noreply.github.com>

--- back/PlanGenBack/src/main/java/com/example/plangenback/controller/PlanQueryController.java
+++ back/PlanGenBack/src/main/java/com/example/plangenback/controller/PlanQueryController.java
@@ -3,11 +3,13 @@ package com.example.plangenback.controller;
 import com.example.plangenback.model.ResponseResult;
 import com.example.plangenback.service.PlanQueryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class PlanQueryController {
     @Autowired
     public PlanQueryService planQueryService;
