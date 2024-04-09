4ec190e79ea1cba9074ac3427d019848b6852865
Inception02 <127276038+Inception02@users.noreply.github.com>
2024-04-09 14:00:26 +0800

    modified two services

--- back/PlanGenBack/src/main/java/com/example/plangenback/service/PlanQueryService.java
+++ back/PlanGenBack/src/main/java/com/example/plangenback/service/PlanQueryService.java
@@ -3,7 +3,9 @@ package com.example.plangenback.service;
 
 import com.example.plangenback.model.ResponseResult;
 
+import java.util.List;
+
 public interface PlanQueryService {
     public ResponseResult getAllTitleByCityAndDisaster(String city, String disaster);
-    public ResponseResult getTextByTitle(Integer documentId, String title);
+    public ResponseResult getTextByTitle(Integer documentId, List<String> title);
 }
