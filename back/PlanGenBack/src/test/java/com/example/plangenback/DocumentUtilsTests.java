c9e1365536553d38a64d98867ce038d197c0256a
Inception02 <127276038+Inception02@users.noreply.github.com>
2024-05-20 23:51:53 +0800

    将接口改为接受文件，保留接受url

---
+++ back/PlanGenBack/src/test/java/com/example/plangenback/DocumentUtilsTests.java
@@ -0,0 +1,27 @@
+package com.example.plangenback;
+
+import com.example.plangenback.service.FileProcessingService;
+import com.example.plangenback.utils.DocumentUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DocumentUtilsTests {
+
+    @Test
+    public void readDocumentUtilTest() {
+//        System.out.println(DocumentUtils.readDocument(".\\planFiles\\flood_plan_undefined2.docx"));
+    }
+
+    @Autowired
+    private FileProcessingService fileProcessingService;
+    @Test
+    public void addDocumentTest() {
+//        String mainTitle = "洪涝灾害预案";
+//        String city = "南京市";
+//        String disaster = "洪水";
+//        String filePath = ".\\planFiles\\flood_plan_undefined2.docx";
+//        System.out.println(fileProcessingService.addFile(mainTitle, city, disaster, DocumentUtils.readDocument(filePath)));
+    }
+}
