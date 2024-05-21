ac60eb7bfaa0339e8d1f0a8b542cc8403dcb18a3
Inception02 <127276038+Inception02@users.noreply.github.com>
2024-05-21 10:17:39 +0800

    解决文件传输跨域问题

---
+++ back/PlanGenBack/src/main/java/com/example/plangenback/config/MyCorsFilter.java
@@ -0,0 +1,23 @@
+package com.example.plangenback.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class MyCorsFilter {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        //这句不加不能跨域上传文件，
+        corsConfiguration.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
+    }
+}
